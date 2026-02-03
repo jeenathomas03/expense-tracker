f01eeaf491172209a36a63daea90932b147dbb48
jeenathomas03 <jeenathomas53@gmail.com>
2026-02-03 11:32:08 +0530

    new updates

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/add")
+def add_expense():
+    return render_template("add_expense.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
